fd46a95ce791d258a7a8881806c3876fa242984b
CCMeta <ccmeta@icloud.com>
2023-09-01 14:56:33 +0800

    add crate cc; auto build c file and make lib

--- openwrt_hello_rust/build.rs
+++ openwrt_hello_rust/build.rs
@@ -1,13 +1,20 @@
+extern crate cc;
+
 fn main() {
-    println!("cargo:rustc-link-search=./"); //指定库
+    cc::Build::new()
+        .file("lib/hello.c")
+        .compile("libhello.a");
+    println!("cargo:rustc-link-search=./lib"); //指定库
+
     // println!("cargo:rustc-link-lib=hello"); //指定库
     println!("cargo:rustc-link-lib=static=hello"); //指定库
+
     // println!("cargo:rerun-if-changed=lib/hello.h");
-    // need to copy libhello.so to /lib 
+    // need to copy libhello.so to /lib
     // export LD_LIBRARY_PATH=/home/z/openwrt_hello_rust/lib:$LD_LIBRARY_PATH:$LD_LIBRARY_PATH
 
     let bindings = bindgen::Builder::default()
-        .header("./lib/hello.h") //指定头文件，可以指定多个.h文件作为输入
+        .header("lib/hello.h") //指定头文件，可以指定多个.h文件作为输入
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
